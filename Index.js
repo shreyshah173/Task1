922fd575b3db7f8b6341cb25c63b159938253add
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-08-31 22:16:42 +0530

    chore(backend):Just basic changes

--- Index.js
+++ Index.js
@@ -1,17 +1,17 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
-const authRoutes = require('./routes/authRoutes');
+const authRoutes=require('./routes/authRoutes');
+const documentRoutes=require('./routes/documentRoutes');
 const app=express();
 dotenv.config();
-
 
 app.use(express.json());
 
 // console.log()
 
 mongoose.connect(process.env.MongoUrl).then(()=>{
-    console.log("MongoDB Connected")
+    // console.log("MongoDB Connected")
 }).catch((err)=>{
     console.log(err)
 })
@@ -21,5 +21,5 @@ app.use('/api/auth', authRoutes);
 
 const PORT=process.env.PORT || 8800;
 app.listen(PORT,()=>{
-    console.log(`User Service Up and Running ${PORT}`)
+    // console.log(`User Service Up and Running ${PORT}`)
 })
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,46 @@
-const User=require('../models/User')
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-exports.register=async(req,res)=>{
-    const {username,password,role}=req.body;
-    try{
-        const existingUser=await User.findOne({username});
-        if(existingUser){
+exports.register = async (req, res) => {
+    const { username, password, role } = req.body;
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
             return res.status(400).json("User Already Exists")
         }
-        const hashedPassword=await bcrypt.hash(password,10);
-        const user=new User({
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
             username,
-            password:hashedPassword,
+            password: hashedPassword,
             role
         })
         await user.save();
         res.status(201).json("User Created Successfully")
     }
-    catch(err){
-res.status(500).json({
-    message:'server error'
-})
+    catch (err) {
+        res.status(500).json({
+            message: 'server error'
+        })
     }
 }
 
-exports.login=async(req,res)=>{
-    const {username,password}=req.body;
-    try{
-        const user=await User.findOne({username})
-        if(!user){
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username })
+        if (!user) {
             return res.status(400).json("User Not Found")
         }
-        const isMatch=await bcrypt.compare(password,user.password);
-        if(!isMatch){
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
             return res.status(400).json("Invalid Credentials")
         }
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        res.status(200).json({token});
-    
+        res.status(200).json({ token });
+
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 }
