1ad64748237d895da774e9cd88251bece11fddf5
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-08-31 21:50:19 +0530

    chore(backend):Fixed api for login and token

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,29 +24,23 @@ res.status(500).json({
 })
     }
 }
+
 exports.login=async(req,res)=>{
     const {username,password}=req.body;
     try{
         const user=await User.findOne({username})
-        console.log("123")
         if(!user){
             return res.status(400).json("User Not Found")
         }
-        console.log("456")
         const isMatch=await bcrypt.compare(password,user.password);
         if(!isMatch){
             return res.status(400).json("Invalid Credentials")
         }
-        console.log("789")
-        // const token=jwt.sign({if:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:'1h'})
-        console.log("101112")
-        console.log(token)
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.status(200).json({token});
     
     }
     catch(err){
-        res.status(500).json({
-            message:'server error'
-        })
+        res.status(500).json(err)
     }
 }
