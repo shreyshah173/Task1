82f7202d7bc6beb2597ceb5f3e246f11cac11b48
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-08-31 14:00:09 +0530

    chore(Backend): Working register endpoint for the user

---
+++ Index.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const authRoutes = require('./routes/authRoutes');
+const app=express();
+dotenv.config();
+
+
+app.use(express.json());
+
+// console.log()
+
+mongoose.connect(process.env.MongoUrl).then(()=>{
+    console.log("MongoDB Connected")
+}).catch((err)=>{
+    console.log(err)
+})
+
+app.use('/api/auth', authRoutes);
+
+
+const PORT=process.env.PORT || 8800;
+app.listen(PORT,()=>{
+    console.log(`User Service Up and Running ${PORT}`)
+})
---
+++ controllers/authController.js
@@ -0,0 +1,52 @@
+const User=require('../models/User')
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
+
+exports.register=async(req,res)=>{
+    const {username,password,role}=req.body;
+    try{
+        const existingUser=await User.findOne({username});
+        if(existingUser){
+            return res.status(400).json("User Already Exists")
+        }
+        const hashedPassword=await bcrypt.hash(password,10);
+        const user=new User({
+            username,
+            password:hashedPassword,
+            role
+        })
+        await user.save();
+        res.status(201).json("User Created Successfully")
+    }
+    catch(err){
+res.status(500).json({
+    message:'server error'
+})
+    }
+}
+exports.login=async(req,res)=>{
+    const {username,password}=req.body;
+    try{
+        const user=await User.findOne({username})
+        console.log("123")
+        if(!user){
+            return res.status(400).json("User Not Found")
+        }
+        console.log("456")
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json("Invalid Credentials")
+        }
+        console.log("789")
+        // const token=jwt.sign({if:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:'1h'})
+        console.log("101112")
+        console.log(token)
+        res.status(200).json({token});
+    
+    }
+    catch(err){
+        res.status(500).json({
+            message:'server error'
+        })
+    }
+}
